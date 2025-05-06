f3e7f5b09e210c121fbfa8fd9a122bcc57a8afce
adesca <adeolubabatunde@gmail.com>
2025-05-06 12:27:17 -0500

    Fix lint issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import {ExerciseTracker} from "./exercise-tracker.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
